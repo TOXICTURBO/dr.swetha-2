ab27497eb840c556d0898ba4148d3f4926e42027
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2026-01-06 01:45:15 +0530

    Create FilmCanvas.tsx

---
+++ components/FilmCanvas.tsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber"
+import { Stars } from "@react-three/drei"
+import CameraRig from "./CameraRig"
+import Fog from "./Fog"
+
+export default function FilmCanvas() {
+  return (
+    <Canvas camera={{ position: [0, 0, 10], fov: 55 }}>
+      <color attach="background" args={["#000"]} />
+      <fog attach="fog" args={["#000", 5, 30]} />
+      <Stars radius={50} depth={40} count={4000} factor={4} fade />
+      <CameraRig />
+      <Fog />
+    </Canvas>
+  )
+}
